8059c2f1b935e368b686bd393d44d93ec7e7f38e
Naeblis <johnmlowery@gmail.com>
2018-11-02 11:05:55 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import PokemonIndex from "./components/PokemonIndex";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokies: []
-    };
-  }
+  state = {
+    pokies: []
+  };
 
   componentDidMount() {
     fetch("http://localhost:3000/pokemon")
       .then(resp => resp.json())
-      .then(pokies => this.setState({ pokies }));
+      .then(data => {
+        this.setState({
+          pokies: data
+        });
+      });
   }
   //
   render() {
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -2,20 +2,43 @@ import React from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import { Search } from "semantic-ui-react";
-import _ from "lodash";
+// import _ from "lodash";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokies: this.props.pokies
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then(resp => resp.json())
+      .then(pokies => this.setState({ pokies: pokies }));
+  }
+
+  search = event => {
+    let newPokies = [];
+    this.props.pokies.filter(poki => {
+      if (poki.name.includes(event.target.value)) {
+        newPokies.push(poki);
+      }
+      this.setState({
+        pokies: newPokies
+      });
+    });
+
+    // 1. filter this.state.pokies to be the updated pokies...
+    // ...list and set it equal to a new variable
+    // 2. after #1, this.setState({ pokies: newPokies })
+  };
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search
-          onSearchChange={_.debounce(() => console.log("🤔"), 500)}
-          showNoResults={false}
-        />
+        <Search onSearchChange={this.search} />
         <br />
-        <PokemonCollection pokies={this.props.pokies} />
+        <PokemonCollection pokies={this.state.pokies} />
         <br />
         <PokemonForm />
       </div>
